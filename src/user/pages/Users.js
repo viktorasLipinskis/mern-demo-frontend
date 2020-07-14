843ff57080ef44fbbe84b0afd5d93a09b8f65e5b
Viktoras Lipinskis <viktoras.lipinskis@gmail.com>
2020-07-14 04:13:35 +0000

    some small fixes

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import UsersList from '../components/UsersList';
+import UsersList from "../components/UsersList";
 
 const Users = () => {
   const USERS = [
     {
-      id: 'u1',
-      name: 'Max Schwarz',
+      id: "u1",
+      name: "Viktoras Lipinskis",
       image:
-        'https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-      places: 3
-    }
+        "https://media-exp1.licdn.com/dms/image/C5103AQEb1E521htbcQ/profile-displayphoto-shrink_400_400/0?e=1600300800&v=beta&t=dknoVSKpbzIi1NBvoOHozwSQvsJN0fyG7MyT1YbLIrU",
+      places: 3,
+    },
   ];
 
   return <UsersList items={USERS} />;
