1db3f538f6ef92908a48dae6e16c3b68a66c7bf5
Viktoras Lipinskis <viktoras.lipinskis@gmail.com>
2020-07-15 04:40:59 +0000

    integrated users with be

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
 import UsersList from "../components/UsersList";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 
 const Users = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const [loadedUsers, setLoadedUsers] = useState();
+  useEffect(() => {
+    const sendRequest = async () => {
+      try {
+        setIsLoading(true);
+        const response = await fetch("http://78.63.9.195:5000/api/users");
+        const responseData = await response.json();
+
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
+        setLoadedUsers(responseData.users);
+        setIsLoading(false);
+      } catch (err) {
+        setIsLoading(false);
+        setError(err.message);
+      }
+    };
+    sendRequest();
+  }, []);
+
+  const errorHandle = () => {
+    setError(null);
+  };
+
   const USERS = [
     {
       id: "u1",
@@ -13,7 +43,19 @@ const Users = () => {
     },
   ];
 
-  return <UsersList items={USERS} />;
+  return (
+    <>
+      <ErrorModal error={error} onClear={errorHandle}></ErrorModal>
+
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+
+      {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+    </>
+  );
 };
 
 export default Users;
