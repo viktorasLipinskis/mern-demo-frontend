34ccd34c0d2b0ff5c1e0525e66a25365f4d02339
Viktoras Lipinskis <viktoras.lipinskis@gmail.com>
2020-07-15 04:41:38 +0000

    removed dublicate code

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -33,16 +33,6 @@ const Users = () => {
     setError(null);
   };
 
-  const USERS = [
-    {
-      id: "u1",
-      name: "Viktoras Lipinskis",
-      image:
-        "https://media-exp1.licdn.com/dms/image/C5103AQEb1E521htbcQ/profile-displayphoto-shrink_400_400/0?e=1600300800&v=beta&t=dknoVSKpbzIi1NBvoOHozwSQvsJN0fyG7MyT1YbLIrU",
-      places: 3,
-    },
-  ];
-
   return (
     <>
       <ErrorModal error={error} onClear={errorHandle}></ErrorModal>
