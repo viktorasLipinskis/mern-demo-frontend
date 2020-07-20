7cdd5f57c546945e21bf94431f706dee634374d7
Viktoras Lipinskis <viktoras.lipinskis@gmail.com>
2020-07-20 04:40:45 +0000

    implementing add place

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -63,7 +63,7 @@ const Auth = () => {
 
     if (isLoginMode) {
       try {
-        await sendRequest(
+        const responseData = await sendRequest(
           "http://78.63.9.195:5000/api/users/login",
           "POST",
           JSON.stringify({
@@ -74,11 +74,11 @@ const Auth = () => {
             "Content-Type": "application/json",
           }
         );
-        auth.login();
+        auth.login(responseData.user.id);
       } catch (err) {}
     } else {
       try {
-        await sendRequest(
+        const responseData = await sendRequest(
           "http://78.63.9.195:5000/api/users/signup",
           "POST",
           JSON.stringify({
@@ -90,7 +90,7 @@ const Auth = () => {
             "Content-Type": "application/json",
           }
         );
-        auth.login();
+        auth.login(responseData.user.id);
       } catch (err) {}
     }
   };
