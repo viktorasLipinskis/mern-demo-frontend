738a8669f842442e738fdb0adfd9093aa1883469
Viktoras Lipinskis <viktoras.lipinskis@gmail.com>
2020-07-26 09:53:58 +0000

    working with token expiration

--- src/App.js
+++ src/App.js
@@ -17,14 +17,17 @@ import { AuthContext } from "./shared/context/auth-context";
 const App = () => {
   const [token, setToken] = useState(false);
   const [userId, setUserId] = useState(false);
-  const login = useCallback((uid, token) => {
+  const login = useCallback((uid, token, expirationDate) => {
     setToken(token);
     setUserId(uid);
+    const tokenExpirationDate =
+      expirationDate || new Date(new Date().getTime(+1000 * 60 * 60));
     localStorage.setItem(
       "userData",
       JSON.stringify({
         userId: uid,
         token,
+        expiration: tokenExpirationDate.toISOString(),
       })
     );
   }, []);
@@ -38,7 +41,11 @@ const App = () => {
   useEffect(() => {
     const storedData = JSON.parse(localStorage.getItem("userData"));
 
-    if (storedData && storedData.token) {
+    if (
+      storedData &&
+      storedData.token &&
+      new Date(storedData.expiration) > new Date()
+    ) {
       login(storedData.userId, storedData.token);
     }
   }, [login]);
